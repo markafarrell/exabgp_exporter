7cacaee0dbd169c1c3474d38e56c24a61b5dc486
Brad Cowie <brad@gizmoguy.net.nz>
2024-02-15 16:58:53 +1300

    Run exabgp inside a python venv.

--- docker/install-and-init.sh
+++ docker/install-and-init.sh
@@ -2,6 +2,9 @@
 
 set -Eeuo pipefail
 
+python3 -m venv /exabgp/venv
+source /exabgp/venv/bin/activate
+
 if [[ "${EXABGP_VERSION}" == "main" ]]; then
     echo "Installing exabgp from git"
     env EXABGP_VERSION="$(date +%Y.%m.%d)" pip3 install git+https://github.com/Exa-Networks/exabgp
